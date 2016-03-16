ada8c071a3d16696d0dc11816958d1de3852299d
vpra <vprasad@umass.edu>
2016-03-16 00:59:08 -0400

    Replaced Rachit's Driver with Vishnu's Driver

--- Driver.java
+++ Driver.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
-public class Driver{
-    public static void main(String[] args){
-    int[][] test = new int[][]{
-      {1,2,3},
-      {2,3,1},
-      {3,1,2}};
+public class Driver {
 
-    Matrix m = new Matrix(test);
-    for(int i: m.getRow(1))
-      System.out.println(i);
-  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float[] row1 = new float[]{0,1,3,4,5,6};
+		float[] row2 = new float[]{0,0,1,5,6,1};
+		float[] row3 = new float[]{1,4,0,6,1,2};
+		float[] row4 = new float[]{1,5,6,1,2,3};
+		float[] row5 = new float[]{0,0,0,1,3,4};
+		float[] row6 = new float[]{1,1,2,3,4,5};
+		float[][] array = new float[][]{row1,row2,row3,row4,row5,row6};
+		Matrix matrix= new Matrix(array);
+		matrix = matrix.rowReduce();
+		matrix.display();
+	}
+
 }

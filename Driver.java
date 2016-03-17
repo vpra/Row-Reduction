fe211587508b6cc64a2eff385b930633904a2d63
vpra <vprasad@umass.edu>
2016-03-16 22:37:28 -0400

    Added Primitive Driver

--- Driver.java
+++ Driver.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		float[] row1 = new float[]{0,1,3,4,5,6};
-		float[] row2 = new float[]{0,0,1,5,6,1};
-		float[] row3 = new float[]{1,4,0,6,1,2};
-		float[] row4 = new float[]{1,5,6,1,2,3};
-		float[] row5 = new float[]{0,0,0,1,3,4};
-		float[] row6 = new float[]{1,1,2,3,4,5};
-		float[][] array = new float[][]{row1,row2,row3,row4,row5,row6};
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter number of rows");
+		int rows = scan.nextInt();
+		System.out.println("Enter number of columns");
+		int columns = scan.nextInt();
+		System.out.println("Enter numbers row by row");
+		float[][] array = new float[rows][columns];
+		for(int x = 0; x<rows; x++){
+			for(int y = 0; y<columns; y++){
+				float input = scan.nextFloat();
+				array[x][y] = input;
+			}
+		}
 		Matrix matrix= new Matrix(array);
 		matrix = matrix.rowReduce();
 		matrix.display();
 	}
 
 }
+
